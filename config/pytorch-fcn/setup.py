eafc58b66ca5c543c6584b823d1ac08950030f44
cuishuhao <601664810@qq.com>
2018-09-01 16:37:40 +0800

    fcn&dataset

---
+++ config/pytorch-fcn/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import shlex
+import subprocess
+import sys
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+__version__ = '1.9.0'
+
+
+if sys.argv[-1] == 'release':
+    commands = [
+        'git tag v{}'.format(__version__),
+        'git push origin master --tags',
+        'python setup.py sdist upload',
+    ]
+    for cmd in commands:
+        subprocess.call(shlex.split(cmd))
+    sys.exit(0)
+
+
+setup(
+    name='torchfcn',
+    version=__version__,
+    packages=find_packages(),
+    install_requires=[r.strip() for r in open('requirements.txt')],
+    description='PyTorch Implementation of Fully Convolutional Networks.',
+    package_data={'torchfcn': ['ext/*']},
+    include_package_data=True,
+    author='Kentaro Wada',
+    author_email='www.kentaro.wada@gmail.com',
+    license='MIT',
+    url='https://github.com/wkentaro/pytorch-fcn',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)

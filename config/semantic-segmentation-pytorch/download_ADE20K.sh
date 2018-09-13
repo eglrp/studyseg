0ae9aa82a9dd04673e9689095a3ab7bf37ecc1c6
cuishuhao <601664810@qq.com>
2018-09-13 08:12:13 +0800

    pspnet

---
+++ config/semantic-segmentation-pytorch/download_ADE20K.sh
@@ -0,0 +1,7 @@
+#wget -O ~/data/datasets/scene/ADEChallengeData2016.zip http://data.csail.mit.edu/places/ADEchallenge/ADEChallengeData2016.zip
+#unzip ~/data/datasets/scene/ADEChallengeData2016.zip -d ~/data/datasets/scene
+#rm ./data/ADEChallengeData2016.zip
+#echo "Dataset downloaded."
+
+wget -O ~/data/datasets/scene/release_test.zip http://data.csail.mit.edu/places/ADEchallenge/release_test.zip
+unzip ~/data/datasets/scene/release_test.zip -d ~/data/datasets/scene
